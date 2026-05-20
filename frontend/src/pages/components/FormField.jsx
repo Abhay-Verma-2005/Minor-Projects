af2c97c9c7c500bc89e82b5b847be11f94e1cc3f
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2026-05-20 19:16:10 +0530

    Delete frontend directory

--- frontend/src/pages/components/FormField.jsx
+++
@@ -1,8 +0,0 @@
-const FormField = ({ label, name, value, onChange, placeholder, type = "text", required, min, step }) => (
-  <div className="field">
-    <label>{label}</label>
-    <input name={name} value={value} onChange={onChange} placeholder={placeholder} type={type} required={required} min={min} step={step} />
-  </div>
-);
-
-export default FormField;
